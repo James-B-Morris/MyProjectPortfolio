e51dbd3a17134cfa9129e10f1a4fee857d501119
jamesmozz11 <jamesmozz11@hotmail.co.uk>
2023-05-19 20:40:29 +0100

    Calculator Interface
    + answer txtView
    + new input txtView
    + input buttons

--- app/src/main/java/com/jamesmozz11/myprojectportfolio/CalculatorActivity.kt
+++ app/src/main/java/com/jamesmozz11/myprojectportfolio/CalculatorActivity.kt
@@ -2,11 +2,17 @@ package com.jamesmozz11.myprojectportfolio
 
 import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
 class CalculatorActivity : AppCompatActivity() {
+    private var answer = 0
+    private var calc = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -14,6 +20,32 @@ class CalculatorActivity : AppCompatActivity() {
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 
         initToolbar()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val btnOne = findViewById<Button>(R.id.btn0)
+        btnOne.setOnClickListener {
+            inputNum(0)
+        }
+    }
+
+    ///// ANSWER CALCULATIONS /////
+    fun inputNum(number: Int) {
+        val txtAnswer = findViewById<TextView>(R.id.txtAns)
+        var txt = txtAnswer.text as String
+
+        if (txt == "0") {
+            return
+        }
+        else if (txt.length >= 10) {
+            // set the last char to the new input
+        }
+        else {
+            txtAnswer.text = "$txt$number"
+            Log.i(getString(R.string.log_calculator), txtAnswer.text as String)
+        }
     }
 
     ///// TOOLBAR STUFF /////
