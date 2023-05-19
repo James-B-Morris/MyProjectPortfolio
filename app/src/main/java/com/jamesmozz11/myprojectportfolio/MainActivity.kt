302a39bab2cc885b56608bf8524bffdf8c437323
jamesmozz11 <jamesmozz11@hotmail.co.uk>
2023-05-19 16:25:38 +0100

    Initial commit

---
+++ app/src/main/java/com/jamesmozz11/myprojectportfolio/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jamesmozz11.myprojectportfolio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
