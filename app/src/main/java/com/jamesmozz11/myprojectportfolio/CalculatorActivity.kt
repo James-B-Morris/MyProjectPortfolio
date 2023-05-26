7a77c5f4c466a72149bb8fba27f85f6123968d70
jamesmozz11 <jamesmozz11@hotmail.co.uk>
2023-05-26 14:09:37 +0100

    Number Inputs
    + number 0-9 can be inputed into the answer view
    + the answer view can be cleared
    - resized the buttons to fit the testing device.
    
    *May need to put the buttons on a fragment, so they change size according to the device

--- app/src/main/java/com/jamesmozz11/myprojectportfolio/CalculatorActivity.kt
+++ app/src/main/java/com/jamesmozz11/myprojectportfolio/CalculatorActivity.kt
@@ -4,6 +4,7 @@ import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -25,9 +26,67 @@ class CalculatorActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
 
-        val btnOne = findViewById<Button>(R.id.btn0)
-        btnOne.setOnClickListener {
+        val txtAns = findViewById<TextView>(R.id.txtAns)
+
+        val btn0 = findViewById<Button>(R.id.btn0)
+        val btn1 = findViewById<Button>(R.id.btn1)
+        val btn2 = findViewById<Button>(R.id.btn2)
+        val btn3 = findViewById<Button>(R.id.btn3)
+        val btn4 = findViewById<Button>(R.id.btn4)
+        val btn5 = findViewById<Button>(R.id.btn5)
+        val btn6 = findViewById<Button>(R.id.btn6)
+        val btn7 = findViewById<Button>(R.id.btn7)
+        val btn8 = findViewById<Button>(R.id.btn8)
+        val btn9 = findViewById<Button>(R.id.btn9)
+
+        val btnDecimal = findViewById<Button>(R.id.btnDecimal)
+        val btnNegative = findViewById<Button>(R.id.btnNegative)
+
+        val btnAdd = findViewById<Button>(R.id.btnAdd)
+        val btnSubtract = findViewById<Button>(R.id.btnSubtract)
+        val btnMultiply = findViewById<Button>(R.id.btnMultiply)
+        val btnEquals = findViewById<Button>(R.id.btnEquals)
+        val btnBracket = findViewById<Button>(R.id.btnBracket)
+        val btnPercent = findViewById<Button>(R.id.btnPercent)
+        val btnClear = findViewById<Button>(R.id.btnClear)
+
+
+        btn0.setOnClickListener {
             inputNum(0)
+        }
+        btn1.setOnClickListener {
+            inputNum(1)
+        }
+        btn2.setOnClickListener {
+            inputNum(2)
+        }
+        btn3.setOnClickListener {
+            inputNum(3)
+        }
+        btn4.setOnClickListener {
+            inputNum(4)
+        }
+        btn5.setOnClickListener {
+            inputNum(5)
+        }
+        btn6.setOnClickListener {
+            inputNum(6)
+        }
+        btn7.setOnClickListener {
+            inputNum(7)
+        }
+        btn8.setOnClickListener {
+            inputNum(8)
+        }
+        btn9.setOnClickListener {
+            inputNum(9)
+        }
+
+        // reset the calculator to default
+        btnClear.setOnClickListener {
+            txtAns.text = "0"
+            answer = 0
+            calc = 0
         }
     }
 
@@ -36,15 +95,24 @@ class CalculatorActivity : AppCompatActivity() {
         val txtAnswer = findViewById<TextView>(R.id.txtAns)
         var txt = txtAnswer.text as String
 
-        if (txt == "0") {
+        if ((txt == "0") && (number == 0)) {
             return
         }
-        else if (txt.length >= 10) {
+        else if (txt.length >= 12) {
             // set the last char to the new input
         }
         else {
-            txtAnswer.text = "$txt$number"
-            Log.i(getString(R.string.log_calculator), txtAnswer.text as String)
+            if (txt == "0") {
+                txtAnswer.text = "$number"
+            }
+            else {
+                // add commas to seperate numbers?
+                //if (3.mod(txtAnswer.length()) == 0) {
+                //    txtAnswer.text = "$txt$number"
+                //}
+                txtAnswer.text = "$txt$number"
+                Log.i(getString(R.string.log_calculator), txtAnswer.text as String)
+            }
         }
     }
 
